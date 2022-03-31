cd8c727d5ec99589d2832b6d6c379fcaa4f37f4f
Leonardo Rodrigues <leo-feitosa@live.com>
2022-03-31 19:02:08 -0300

    Conectando ao Banco de Dados

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -17,7 +17,7 @@ namespace FilmesAPI.Controllers
 
         [HttpPost] 
         public IActionResult AdicionaFilme([FromBody] Filme filme) 
-        { // Boa prática é retornar o caminho para ao recurso que está criando
+        {
             filme.Id = id ++;
             filmes.Add(filme);
             return CreatedAtAction(nameof(RecuperarFilmesPorId), new {Id = filme.Id}, filme);
@@ -25,13 +25,13 @@ namespace FilmesAPI.Controllers
 
         [HttpGet]
         public IActionResult RecuperaFilmes()
-        { //como o usuario n esta requerindo é uma boa prática retornar uma lista vazia
+        {
             return Ok(filmes);
         }
 
         [HttpGet("{id}")]
         public IActionResult RecuperarFilmesPorId(int id)
-        { //problema se por acaso o filme não for encontrado
+        {
             var filme =  filmes.FirstOrDefault(filme => filme.Id == id);
             if(filme != null)
             {
--- Startup.cs
+++ Startup.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+
+using FilmesAPI.Data;
 
 namespace FilmesAPI
 {
@@ -26,7 +29,7 @@ namespace FilmesAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<FilmeContext>(opts => opts.UseMySQL(Configuration.GetConnectionString("FilmeConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
