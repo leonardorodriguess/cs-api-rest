eb5c9e95be380243cb751e321a62aa56eaed7f20
Leonardo Rodrigues <leo-feitosa@live.com>
2022-04-01 17:56:59 -0300

    Usando Data Transfer Objects

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FilmesAPI.Data;
+using FilmesAPI.Data.DbContextOptions;
 using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using static System.Console;
@@ -21,8 +22,15 @@ namespace FilmesAPI.Controllers
         }
 
         [HttpPost] 
-        public IActionResult AdicionaFilme([FromBody] Filme filme) 
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto) 
         {
+            Filme filme = new Filme 
+            {
+                Titulo = filmeDto.Titulo,
+                Genero = filmeDto.Genero,
+                Diretor = filmeDto.Diretor,
+                Duracao = filmeDto.Duracao
+            };
             _context.Filmes.Add(filme);
             _context.SaveChanges();
             return CreatedAtAction(nameof(RecuperarFilmesPorId), new {Id = filme.Id}, filme);
@@ -37,16 +45,25 @@ namespace FilmesAPI.Controllers
         [HttpGet("{id}")]
         public IActionResult RecuperarFilmesPorId(int id)
         {
-            var filme =  _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            Filme filme =  _context.Filmes.FirstOrDefault(filme => filme.Id == id);
             if(filme != null)
             {
-                return Ok(filme);
+                ReadFilmeDto filmeDto = new ReadFilmeDto
+                {
+                    Titulo = filme.Titulo,
+                    Diretor = filme.Diretor,
+                    Duracao = filme.Duracao,
+                    Id = filme.Id,
+                    Genero = filme.Genero,
+                    HoraDaConsulta = DateTime.Now
+                };
+                return Ok(filmeDto);
             }
             return NotFound();
         }
 
         [HttpPut("{id}")]
-        public IActionResult AtualizaFilme(int id, [FromBody] Filme filmeNovo)
+        public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto FilmeDto)
         {
             Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
             if(filme == null)
@@ -54,10 +71,10 @@ namespace FilmesAPI.Controllers
                 return NotFound();
             }
 
-            filme.Titulo = filmeNovo.Titulo;
-            filme.Genero = filmeNovo.Genero;
-            filme.Duracao = filmeNovo.Duracao;
-            filme.Diretor = filmeNovo.Diretor;
+            filme.Titulo = FilmeDto.Titulo;
+            filme.Genero = FilmeDto.Genero;
+            filme.Duracao = FilmeDto.Duracao;
+            filme.Diretor = FilmeDto.Diretor;
             _context.SaveChanges();
             return NoContent(); 
         }
