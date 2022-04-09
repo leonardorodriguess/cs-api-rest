6d344e62a7711702e4e1e29804faeff72ee3f299
Leonardo Rodrigues <leo-feitosa@live.com>
2022-04-09 15:39:58 -0300

    Criando relação entre Gerente e Cinemas

--- Migrations/20220409183822_adicionando relação entre gerente e cinema.Designer.cs
+++ Migrations/20220409183822_adicionando relação entre gerente e cinema.Designer.cs
@@ -2,14 +2,16 @@
 using FilmesAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FilmesAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220409183822_adicionando relação entre gerente e cinema")]
+    partial class adicionandorelaçãoentregerenteecinema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
